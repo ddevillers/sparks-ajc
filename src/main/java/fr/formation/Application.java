7e4f4bb102679baaf588ce7ab1217a460ae24d5e
Lalie <modestecoralie@orange.fr>
2020-04-20 17:07:39 +0200

    Modif
    Cora

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -1,9 +1,14 @@
 package fr.formation;
 
 public class Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello Woorld!");
+<<<<<<< Updated upstream
 		
 		System.out.println("Del");
+=======
+		System.out.println("Coralie");
+>>>>>>> Stashed changes
 	}
 }
