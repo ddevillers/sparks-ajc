0c5ba18b40cba44903a2945589d6abdbbcc67a54
Jeremy <jeremy.perrouault@ascadis.fr>
2020-04-20 17:19:46 +0200

    Jeremy

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -4,5 +4,6 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello Woor!d!");
 		System.out.println("Adrien, pas lui, l'autre");
+		System.out.println("Jérémy");
 	}
 }
