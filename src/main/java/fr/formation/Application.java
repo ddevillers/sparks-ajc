f95222301e8e25e82f9b9608c2b45ceb1915f002
PPJJ <ppjj.fr@gmail.com>
2020-04-20 17:07:01 +0200

    HVGUYKFtyufty

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -3,5 +3,6 @@ package fr.formation;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello Woor!d!");
+		System.out.println("Julien : mais what a bordel !");
 	}
 }
