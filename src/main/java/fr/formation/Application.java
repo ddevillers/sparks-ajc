94768af3e5c303a194de9434134d5ec3f1da20e0
Jeremy <jeremy.perrouault@ascadis.fr>
2020-04-20 17:13:30 +0200

    Reclean

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -1,18 +1,7 @@
 package fr.formation;
 
 public class Application {
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.println("Hello Woor!d!");
-		System.out.println("Julien : mais what a bordel !");
-=======
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello Woorld!");
-
-		System.out.println("Del");
-		System.out.println("Coralie");
-		System.out.println("Xalleboss");
-		System.out.println("Anthony");
 	}
 }
