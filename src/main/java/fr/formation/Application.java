b697903a33da6fd1156341200ee884cd969ea89b
Delphine <del.devillers@gmail.com>
2020-04-20 17:06:38 +0200

    Modif Del

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -3,7 +3,7 @@ package fr.formation;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello Woorld!");
-		System.out.println("ON VA SE MARRER LOL");
-		System.out.println("L'autre Adrien");
+		
+		System.out.println("Del");
 	}
 }
