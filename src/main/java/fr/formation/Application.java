a4595ea5a74c55b3b153c839f255bb253bceff89
Lalie <modestecoralie@orange.fr>
2020-04-20 17:24:58 +0200

    Coralie
    1

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -3,7 +3,11 @@ package fr.formation;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello Woor!d!");
+<<<<<<< Updated upstream
 		System.out.println("❤");
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 		
 		
 		System.out.println("Del");
@@ -14,6 +18,7 @@ public class Application {
 		
 		
 		
+		System.out.println("Coralie");
 		
 		
 		
@@ -37,7 +42,22 @@ public class Application {
 		
 		
 		
+<<<<<<< Updated upstream
 		System.out.println("Jérémy");
 		
+=======
+		
+		
+		
+=======
+		System.out.println("Adrien, pas lui, l'autre");
+<<<<<<< Updated upstream
+		System.out.println("Jérémy");
+=======
+		System.out.println("Hello World!");
+		System.out.println("Coralie");
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 	}
 }
