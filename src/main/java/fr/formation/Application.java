bf2d8b1b8856fb52ac132d96d07866722013a231
AdrienB <boubee.adrien@gmail.com>
2020-04-20 17:13:52 +0200

    push1

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -3,5 +3,6 @@ package fr.formation;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello Woor!d!");
+		System.out.println("Adrien, pas lui, l'autre");
 	}
 }
