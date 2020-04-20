9efee02c3f18263cc67a0c01fa250e7765bceddd
Jordan <jordanabid@gmail.com>
2020-04-20 17:36:52 +0200

    SVP

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -3,7 +3,7 @@ package fr.formation;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello Woor!d!");
-		System.out.println("#JESUISJORDAN");
+		System.out.println("#JESUISJORDAN#DROPAKEYPLZ");
 		
 		System.out.println("Kodo");
 		
